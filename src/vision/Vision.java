a30afeba656b88ccab22b6a1bb3d83034331fc60
Sam Heather <sam@heather.sh>
2014-01-24 22:30:36 +0000

    Initial commit of workspace

---
+++ src/vision/Vision.java
@@ -0,0 +1,17 @@
+package vision;
+
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.util.glu.GLU;
+import org.lwjgl.input.Keyboard;
+
+public class Vision {
+
+	public Vision() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+}
